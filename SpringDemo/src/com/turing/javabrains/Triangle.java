e9417ab6355b081c4b422f91a2826e2738d97554
ThaddeusLincolnStevens <beny1419@yahoo.es>
2016-08-01 22:48:57 +0200

    If we add a 3rd constructor, which takes only 1 parameter ("height" in this case), as we haven't specified the data type Spring has no way to determine which constructor to use, so we're forced to specify the data type in <constructor-arg>

--- SpringDemo/src/com/turing/javabrains/Triangle.java
+++ SpringDemo/src/com/turing/javabrains/Triangle.java
@@ -3,11 +3,25 @@ package com.turing.javabrains;
 public class Triangle {
 	
 	private String type;
+	private int  height;
 	
 	public Triangle(String type) {
 		super();
 		this.type = type;
 	}
+
+	
+	public Triangle(String type, int height) {
+		super();
+		this.type = type;
+		this.height = height;
+	}
+	
+	public Triangle(int height) {
+		super();
+		this.height = height;
+	}
+
 
 	public String getType() {
 		return type;
@@ -18,7 +32,11 @@ public class Triangle {
 	}*/
 	
 	public void draw(){
-		System.out.println(getType() + ": Triangle Draw");
+		System.out.println(getType() + ": Triangle Draw and with a height of " + getHeight());
 		
+	}
+
+	public int getHeight() {
+		return height;
 	}
 }
